c374dd030d47c1d03520912d1858ba1350aebfff
Jack <zjgsws@gmail.com>
2021-09-03 23:37:06 -0400

    ispalindrome_linkedlist

---
+++ leetcodes/isPalindrome_linkedlist.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        values = []
+        while head:
+            values.append(head.val)
+            head = head.next
+        
+        for i in range(len(values)):
+            if values[i] != values[-(i+1)]:
+                return False
+        return True
