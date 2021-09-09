07d40903597910dd6654f873cac298891cc406d7
Jack <zjgsws@gmail.com>
2021-09-08 21:59:38 -0400

    missing num

---
+++ leetcodes/missing_num.py
@@ -0,0 +1,9 @@
+
+# missing number regular solution
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
+
+# improve by divide and conquer
